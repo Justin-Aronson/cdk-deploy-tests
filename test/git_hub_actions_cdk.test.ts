c5e256824ee814799abf6de14db20ab096f57c29
JustinAronson <justinaronson@gmail.com>
2024-11-18 14:22:29 -0600

    Initial commit

---
+++ test/git_hub_actions_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GitHubActionsCdk from '../lib/git_hub_actions_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/git_hub_actions_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GitHubActionsCdk.GitHubActionsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
